23eba5cd9da02ae7c2ff7567c413644cb7406313
Tatu Anttila <tatu.anttila@tuni.fi>
2023-06-07 14:07:39 +0300

    harjoitukset 3.15-3.18

--- index.js
+++ index.js
@@ -1,114 +1,126 @@
+require('dotenv').config() //ympäristömuuttujat
 const express = require('express')
-var morgan = require('morgan')
-
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-app.use(express.json()) // tarvitaan JSON datan käsittelyyn
-app.use(cors())
-// tarkista, löytyykö 'build' kansiosta vastaava tiedosto:
+const Person = require('./models/person') // mongoDB model
+
+
+// middleware: tarkista, löytyykö 'build' kansiosta vastaava tiedosto
 app.use(express.static('build')) 
+// middleware: tarvitaan JSON datan käsittelyyn
+app.use(express.json()) 
+// middleware: Cross-origin resource sharing
+app.use(cors())
+
+// middleware: olemattomien osoitteiden käsittely
+// HUOM: otetaan käyttöön vasta koodin lopussa, muuten sotkee virheilmoituksia
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // uusi token (morgan): 
 morgan.token('body', function getBody (req) {
     return (JSON.stringify(req.body))
   })
-
-// käytetään seuraavaa formaattia morganin tuottamissa lokeissa
+// middleware: käytetään seuraavaa formaattia morganin tuottamissa lokeissa
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons= [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-    }
-  ]
+// virheidenkäsittelijä
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-// palauta kaikki
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  // siirry Expressin virheidenhallintaan:
+  next(error)
+}
+
+
+// palauta kaikki resurssit:
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
+})
 
-// info
+// info:
 app.get('/info', (req, res) => {
-    let dd= new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>
-        <p>${dd}</p>`)    
+    let currentDate= new Date()
+    let noPeople = 0
+  
+    Person.find({}).then(result => {
+      result.forEach(person=> noPeople++)    
+      res.send(`<p>Phonebook has info for ${noPeople} people</p>
+        <p>${currentDate}</p>`)    
   })
+})
 
-// palauta resurssi id:n perusteella
-app.get('/api/persons/:id', (request, response) => {
-    // muunna numeroksi (muuten vertailu (ks. alla) ei toimi)
-    const id = Number(request.params.id) 
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else { // 404 jos resurssia ei löydy
-      // end ilmoittaa siitä, että pyyntöön tulee vastata ilman dataa
-      response.status(404).end()
-    }
-  })
+// palauta resurssi id:n perusteella:
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    // käsittely siirtyy  virheidenkäsittelymiddlewarelle:
+    .catch(error => next(error)) 
+})
 
-// poista resurssi
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons= persons.filter(person => person.id !== id)  
-    response.status(204).end()
-  })
+// poista resurssi:
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-// uuden resurssin lisäys
+// uuden resurssin lisäys:
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log('error666')
-    // tarkista että nimi ja numero on annettu
+    // tarkista että sekä nimi että numero on annettu
     if (!body.name || !body.number) {
-      console.log('error666')
       return response.status(400).json({ 
         error: 'name or number is missing' 
       })
     }
-    // tarkista, ettei numeroa ole jo luettelossa
-    const number = persons.find(person => person.number === body.number)
-    if (number) {
-        return response.status(400).json({ 
-            error: 'number already exists in the phonebook' 
-          })
-    }
-    // luo objekti
-    const person = {
+    // luo objekti ja talleta se tietokantaan
+    const personDB = new Person({
       name: body.name,
       number: body.number,
-      id: generateId(),
-    }
-    persons = persons.concat(person)  
-    response.json(person)
+    })
+      personDB.save().then(savedPerson => {
+      response.json(savedPerson)      
+    })
+    .catch(error => next(error))
   })
 
-// uuden ID:n generointi
-const generateId = () => {
-    min= 1
-    max= 10000
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+// numeron vaihtaminen:
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
   }
 
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+// virheidenkäsittelijä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen
+app.use(errorHandler)
+
 // const PORT = process.env.PORT || 3001
-const PORT = 3000 // muutettu fly.io:n vaatimusten mukaisesti
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
